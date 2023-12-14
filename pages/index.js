fe02f6c09f9544749f020e5d532e10c1b35f9833
Michael Brantley <brantleyrecs@gmail.com>
2023-12-13 20:59:32 -0600

    SHHH

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,30 @@
 import { Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import Sheep from '../components/dontWorryAboutIt/Sheep';
 
 function Home() {
   const { user } = useAuth();
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.firstName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      <Sheep />
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <h1>Hello {user.fbUser.firstName}! </h1>
+        <p>Your Bio: {user.bio}</p>
+        <p>Click the button below to logout!</p>
+        <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+          Sign Out
+        </Button>
+      </div>
+    </>
   );
 }
 
